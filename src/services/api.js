edb7db78ee038656a67b151d98a34b44128b5844
shashank <mathurshashank2299@gmail.com>
2024-09-02 17:43:18 +0530

    Toystack api Trial

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // Adjust this to your backend URL
+  baseURL: "https://awwalai.toystack.dev/api", // Adjust this to your backend URL
   headers: {
     "Content-Type": "application/json",
   },
